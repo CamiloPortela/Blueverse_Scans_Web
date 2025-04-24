dcefa20ec27c98062c6e2703286056386a11b16e
Imonkep ‎ <camiloportela247@gmail.com>
2025-04-24 16:32:35 -0500

    Se agregaron descripciones en archivo de media y se cambió el nombre de la página vista desde la pestaña del navegador

--- src/data/media.js
+++ src/data/media.js
@@ -3,52 +3,61 @@ const mangas = [
       id: 1,
       title: "Isekai Samurai",
       image: new URL('../assets/isekai_samurai.jpg', import.meta.url).href,
-      description: "Una samurái ruda en busca de venganza. Historia intensa y combates memorables.",
+      description: "La samurái más poderosa de la historia vaga por el mundo en busca de un buen lugar para morir, ¡hasta que, de repente, es transportada a otro mundo! ¡Una samurái invencible se abre paso a través de otro mundo! ¡Una fantasía samurái isekai creada por Keigo Saito con su estilo brillante y arrollador!",
     },
     {
       id: 2,
       title: "Agharta",
-      image: new URL('../assets/agharta08.jpg', import.meta.url).href
+      image: new URL('../assets/agharta08.jpg', import.meta.url).href,
+      description: "Agharta narra la historia de Juju, un adolescente que pertenece a una de las bandas que prevalecen en un mundo desolado tras un cataclismo. El agua potable es escasa y preciada. Juju se encuentra con Rael, una chica que podría ser la clave para salvar a la humanidad, y juntos se embarcan en un viaje a través del desierto.",
     },
     {
       id: 3,
       title: "Batman: La broma asesina",
-      image: new URL('../assets/batman.jpg', import.meta.url).href
+      image: new URL('../assets/batman.jpg', import.meta.url).href,
+      description: "La historia gira en torno a la relación entre Batman y el Joker, explorando su compleja dinámica y la delgada línea que separa la locura de la cordura. La obra es conocida por su narrativa oscura y su enfoque psicológico, así como por su icónica escena en la que el Joker intenta demostrar que cualquier persona puede volverse loca bajo las circunstancias adecuadas.",
     },
     {
       id: 4,
       title: "One Piece",
-      image: new URL('../assets/onepiece.jpg', import.meta.url).href
+      image: new URL('../assets/onepiece.jpg', import.meta.url).href,
+      description: "One Piece sigue las aventuras de Monkey D. Luffy, un joven pirata que busca el tesoro legendario conocido como 'One Piece' para convertirse en el Rey de los Piratas. A lo largo de su viaje, Luffy forma una tripulación diversa y enfrenta numerosos enemigos y desafíos en un mundo lleno de islas, criaturas fantásticas y poderes sobrenaturales.",
     },
     {
       id: 5,
       title: "Shakara",
-      image: new URL('../assets/shakara.jpg', import.meta.url).href
+      image: new URL('../assets/shakara.jpg', import.meta.url).href,
+      description: "Escrito por Robbie Morrison y dibujado por Henry Flint, Shakara sigue las aventuras del personaje a través de la galaxia como un agente de la venganza movido únicamente por el deseo de vengarse de las civilizaciones que destruyeron a la suya destruyendo la monarquía espacial y ganándose muchos enemigos en el camino Shakara descubre que la verdad detrás de la muerte de su especie y lo que la rodea no es lo que parece.",
     },
     {
       id: 6,
       title: "Onani Master Kurosawa",
-      image: new URL('../assets/onani.jpg', import.meta.url).href
+      image: new URL('../assets/onani.jpg', import.meta.url).href,
+      description: "Onani Master Kurosawa sgue la historia de Kakeru Kurosawa que es un joven de secundaria bastante inteligente. Idea planes para ir al lavabo de chicas que hay en la tercera planta y poder masturbarse pensando en todo tipo de fantasías. Aunque alguien acaba por descubrir su secreto y le pedirá que le ayuda a vengarse de sus enemigos, utilizando sus desechos.",
     },
     {
       id: 7,
       title: "Granblue Dreaming",
-      image: new URL('../assets/granblue.jpg', import.meta.url).href
+      image: new URL('../assets/granblue.jpg', import.meta.url).href,
+      description: "Granblue Dreaming cuenta la historia de Iori Kitahara, un joven que se traslada a la península de Izu para comenzar su vida universitaria, alojándose en la habitación de arriba de la tienda de buceo de su tío, llamada 'Grand Blue'. Sin embargo, pronto se encuentra envuelto en las travesuras y fiestas de los miembros del Club de Buceo local, quienes pasan más tiempo bebiendo y divirtiéndose que buceando.",
     },
     {
       id: 8,
       title: "Desire for a Reply!",
-      image: new URL('../assets/desire.jpg', import.meta.url).href
+      image: new URL('../assets/desire.jpg', import.meta.url).href,
+      description: "Sunaki es un jugador sustituto del equipo de baseball. Un día, finalmente encuentra el coraje para poner una carta de amor dentro del casillero de zapatos de la chica ídolo de su escuela, pero ¿hacia dónde lo llevarán sus acciones?",
     },
     {
       id: 9,
       title: "The Walking Dead",
-      image: new URL('../assets/walking.jpg', import.meta.url).href
+      image: new URL('../assets/walking.jpg', import.meta.url).href,
+      description: "The Walking Dead es una serie de cómics que sigue la historia de un grupo de sobrevivientes en un mundo post-apocalíptico infestado de zombis. La trama se centra en Rick Grimes, un exoficial de policía que despierta de un coma para descubrir que el mundo ha sido devastado por una epidemia que convierte a los muertos en caminantes. A medida que Rick intenta reunirse con su familia y encontrar un lugar seguro, se enfrenta a numerosos desafíos, tanto de los zombis como de otros sobrevivientes.",
     },
     {
       id: 10,
       title: "Ranger Reject",
-      image: new URL('../assets/ranger.jpg', import.meta.url).href
+      image: new URL('../assets/ranger.jpg', import.meta.url).href,
+      description: "¡Se desata una lucha a vida o muerte! Por un lado, los Guardianes del Dragón, Rangers que usan el poder de los Dioses Dragón para proteger a la humanidad. Por el otro, el Ejército del Mal, ¡invasores empeñados en la conquista mundial!... ¡Sí, claro, qué farsa! Los invasores del espacio exterior, sometidos a la fuerza durante el primer año de la guerra, se han convertido en tontos bailarines. ¡Existen solo para ser derrotados ante las masas cada domingo! Ya ni siquiera se les puede llamar 'villanos', viven sus días como esclavos. Pero un invasor solitario no lo tolerará más. ¡Se enfrentará a los invencibles Rangers y derribará el sistema! ¡Comienza la primera (y quizás última) serie de acción 'anti-Ranger'! ",
     },
   ];
   
